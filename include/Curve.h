1ef6738a0cf991b2f8623126d48918be8b9e6a7a
Zahkklm <ozgurpeynirci@gmail.com>
2024-09-09 17:54:04 +0300

    first

---
+++ include/Curve.h
@@ -0,0 +1,28 @@
+#ifndef CURVE_H_
+#define CURVE_H_
+class Curve
+    {
+    private:
+        ZP* FField;
+        Element A,B;
+
+    public:
+        Curve();
+        Curve(Integer primeField, Integer A, Integer B);
+        ~Curve();
+        bool isZeroDiscriminant();
+        ZP *getField()
+            {
+            return this->FField;
+            };
+
+        Element getA(){
+            return this->A;
+        };
+        Element getB(){
+            return this->B;
+        };
+        void print(); 
+    };
+
+#endif
